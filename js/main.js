64b080f2946f9b1ab4e1c49cdfafa7cd0f291e86
riyaz1150 <riyaz3068@gmail.com>
2015-08-16 14:50:05 +0530

    My first upload on 16 Aug 2015

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	
+	$('#toggle-view li').click(function () {
+
+		var text = $(this).children('div.panel');
+
+		if (text.is(':hidden')) {
+			text.slideDown('200');
+			$(this).children('span').html('-');		
+		} else {
+			text.slideUp('200');
+			$(this).children('span').html('+');		
+		}
+		
+	});
+
+});
